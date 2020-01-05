68fa5049dd2a406d9ac23cc4b60de7709eb45cd7
JayDalton <github@t-jay.net>
2020-01-05 20:06:00 +0100

    changes

--- CommonFilter/Serialize/JsonReader.cpp
+++ CommonFilter/Serialize/JsonReader.cpp
@@ -1,6 +1,8 @@
 ﻿#include "stdafx.h"
 
+
 #include "JsonReader.h"
+#include <CommonFilter\Config\Visitor.h>
 
 using Buffer = json::IStreamWrapper;
 using Writer = json::PrettyWriter<Buffer>;
@@ -15,56 +17,85 @@ struct JsonParser
    {
       logger::info("Key({0}, {1}, {2})", str, length, copy); 
       m_iter = std::find_if(m_map.begin(), m_map.end(), 
-         [](const auto& pair) { return false; }
+         [str](const auto& pair) { return pair.first == str; }
       );
-      return m_iter != m_map.end();
+      return true;
    }
 
+   bool String(const char* str, json::SizeType length, bool copy) 
+   {
+      if (m_iter == m_map.end())
+      {
+         return false;
+      }
+      if (!std::get_if<StringParameter>(&m_iter->second))
+      {
+         return false;
+      }
+      std::get<StringParameter>(m_iter->second).setCurrent(str);
+      return true;
+   }
 
-   // Handler API
-   bool Null() { logger::info("Null()"); return true; }
-   bool Int64(int64_t i) { logger::info("Int64({})", i); return true; }
-   bool Uint64(uint64_t u) { logger::info("UInt64({})", u); return true; }
+   bool Double(double value) 
+   { 
+      if (m_iter != m_map.end())
+      {
+         if (std::get_if<DoubleParameter>(&m_iter->second))
+         {
+            std::get<DoubleParameter>(m_iter->second).setCurrent(value);
+         }
+      }
+      return true; 
+   }
+
+   bool Bool(bool value) 
+   { 
+      if (m_iter != m_map.end())
+      {
+         if (std::get_if<BooleanParameter>(&m_iter->second))
+         {
+            std::get<BooleanParameter>(m_iter->second).setCurrent(value);
+         }
+      }
+      return true; 
+   }
+
+   bool Int(signed value) { 
+      if (m_iter != m_map.end())
+      {
+         if (std::get_if<IntegerParameter>(&m_iter->second))
+         {
+            std::get<IntegerParameter>(m_iter->second).setCurrent(value);
+         }
+      }
+      return true; 
+   }
+   bool Uint(unsigned value) 
+   { 
+      if (m_iter != m_map.end())
+      {
+         if (std::get_if<IntegerParameter>(&m_iter->second))
+         {
+            std::get<IntegerParameter>(m_iter->second).setCurrent(value);
+         }
+      }
+      return true; 
+   }
+
+   bool Null() 
+   { 
+      logger::info("Null()"); return true; 
+   }
+   bool Int64(int64_t i) 
+   { 
+      logger::info("Int64({})", i); return true; 
+   }
+   bool Uint64(uint64_t u) 
+   { 
+      logger::info("UInt64({})", u); return true; 
+   }
    bool RawNumber(const char* str, json::SizeType length, bool copy) { 
       logger::info("Number({0}, {1}, {2})", str, length, copy); return true;
-   }
-
-   bool Bool(bool b) { 
-
-      //auto& param = m_params.back();
-      //if (std::holds_alternative<bool>(param))
-      //{
-      //   return std::get<bool>(param);
-      //}
-
-
-      //m_params.back().emplace<BaseParameter>();
-      //m_params.back().m_current = b;
-      logger::info("Bool({})", b); 
-      return true; 
-   }
-
-   bool Int(signed i) { 
-      //m_params.back().m_current = i;
-      logger::info("Int({})", i); 
-      return true; 
-   }
-   bool Uint(unsigned u) { 
-      //m_params.back().m_current = u;
-      logger::info("Uint({})", u); 
-      return true; 
-   }
-
-   bool Double(double d) { 
-      //m_params.back().m_current = d;
-      logger::info("Double({})", d); 
-      return true; 
-   }
-
-   bool String(const char* str, json::SizeType length, bool copy) { 
-      //m_params.back().m_current = std::string{str, length};
-      logger::info("String({0}, {1}, {2})", str, length, copy); 
-      return true;
    }
 
    bool StartObject() { logger::info("StartObject()"); return true; }
@@ -101,24 +132,6 @@ JsonReader::JsonReader(std::istream& stream)
    : m(std::make_unique<Impl>(stream))
 {
 }
-
-//JsonReader::JsonReader(std::string_view content)
-//   : m(std::make_unique<Impl>())
-//{
-//
-//}
-//
-//JsonReader::JsonReader(const std::string& content)
-//   : m(std::make_unique<Impl>())
-//{
-//
-//}
-//
-//JsonReader::JsonReader(const fs::path& filePath)
-//   : m(std::make_unique<Impl>())
-//{
-//
-//}
 
 JsonReader::~JsonReader() = default;
 JsonReader::JsonReader(JsonReader&& other) = default;
