86232ee2554230372de54e0e425088d28657808f
Jay Desktop <github@t-jay.net>
2021-02-07 22:15:11 +0100

    + command line args handler

--- FilterConsole/Context.ixx
+++ FilterConsole/Context.ixx
@@ -1,5 +1,14 @@
+module; //begins global module fragment
+
 #include <string>
-//#include <iostream>
+#include <iostream>
+#include <filesystem>
+
+#include <Logger/Logger.h>
+#include <Matrix/FileManager.h>
+#include <Matrix/FilterManager.h>
+#include <Matrix/MatrixManager.h>
+
 
 //import std.core;
 //import std.regex;
@@ -11,16 +20,33 @@ export module Context;
 
 export struct Context
 {
+   Context()
+   {
+      setupLogger();
+   }
+
    std::string getString() const
    {
       return "Hello World!";
    }
 
-   signed getX() const
+private:
+
+   void setupLogger()
    {
-      return x;
+      spdlog::flush_on(spdlog::level::debug);
+      spdlog::set_level(spdlog::level::trace);
+
+      Logger::appendLoggerSink(Logger::createMsvcLoggerSink());
+      Logger::appendLoggerSink(Logger::createFileLoggerSink("console.log"));
+
+      spdlog::info("===============================================");
+      spdlog::info("Logging: {0} - {1}", "Console", "0.0.1");
+      spdlog::info("===============================================");
    }
 
 private:
-   signed x{ 0 };
+   //FileManager m_fileManger;
+   //MatrixManager m_matrixManager;
+   //FilterManager m_filterManager;
 };
--- FilterConsole/main.cpp
+++ FilterConsole/main.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
-#include <vector>
+#include <filesystem>
+
+#include <opencv2/core/utility.hpp>
 
 import Context;
 
-using namespace std;
 
-int main()
+int main(int argc, const char* const argv[])
 {
+   const cv::String keys =
+      "{help h usage ? |      | print this message   }"
+      "{@image         |<none>| image for conversion }"
+      "{result         |.     | path to file         }"
+      "{fps            | -1.0 | fps for output video }"
+      "{N count        |100   | count of objects     }"
+      "{ts timestamp   |      | use time stamp       }"
+      ;
+
+   cv::CommandLineParser parser(argc, argv, keys);
+   parser.about("Application name v1.0.0");
+   if (parser.has("help"))
+   {
+      parser.printMessage();
+      return 0;
+   }
+
+   if (!parser.check())
+   {
+      parser.printErrors();
+      return 0;
+   }
+
+   auto N = parser.get<int>("N");
+   auto fps = parser.get<double>("fps");
+   auto image = parser.get<cv::String>("@image");
+   auto result = parser.get<cv::String>("result");
+
+   namespace fs = std::filesystem;
+   if (!fs::exists(image))
+   {
+      std::cout << "file not found" << std::endl;
+      return 0;
+   }
+
+
    Context context;
-   std::cout << context.getString() << std::endl;
+   std::cout << image << std::endl;
+   std::cout << result << std::endl;
 }
 
